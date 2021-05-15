8f4fa5b6b4847c3e143b22bd6733a8b1379ef871
rmiglin <rossmiglin@gmail.com>
2021-05-15 01:15:23 -0400

    fix toggle colors

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const switcher = document.querySelector('.btn');
 
 switcher.addEventListener('click', function() {
-    document.body.classList.toggle('dark-theme')
+    document.body.classList.toggle("dark-theme");
 
     var className = document.body.className;
     if(className == "light-theme") {
